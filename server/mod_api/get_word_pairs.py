c794f2d5e53c6f57300a47fd4e1c3a5550da88c4
hammad haleem <hammadhaleem@gmail.com>
2017-03-18 19:07:57 +0800

    Initial version of api

--- server/mod_api/get_word_pairs.py
+++ server/mod_api/get_word_pairs.py
@@ -1,25 +1,47 @@
 import pandas as pd
 from __builtin__ import list
+import numpy as np
+
+def for_each_review_(review):
+    del review['_id']
+    scored_terms = review['score']
+    for term in scored_terms.keys():
+        term_list = term.split(" ")
+        for t in term_list:
+            try:
+                scored_terms[term]['frequency'][t] = review['tf_idf'][t]
+
+
+            except:
+                scored_terms[term]['frequency'] = {}
+                scored_terms[term]['frequency'][t] = review['tf_idf'][t]
+                scored_terms[term]['frequency']['polarity'] = np.mean(review['final_pairs'][term])
+
+    review['score'] = scored_terms
+
+    del review['final_pairs']
+    del review['tf_idf']
+    return review
 
 
 def get_word_pairs(review_list, mongo_connection):
     query = {
-        'review_id': {
-            '$in': review_list
+        'business_id': {
+            '$in': ['qJAdvf4yDfr8jMS7NqCEPQ']
         }
     }
     what = {
-        'review_id' : 1,
-        'polarity' : 1,
-        'score' : 1,
-        'business_id' : 1,
-        'stars' : 1
+        'review_id': 1,
+        'polarity': 1,
+        'score': 1,
+        'business_id': 1,
+        'stars': 1,
+        'tf_idf': 1,
+        'final_pairs': 1
     }
 
-    processed = list(mongo_connection.db.yelp_review_scored_pairs.find(query,what))
-    for raw in processed:
-        del raw['_id']
-
+    processed = list(mongo_connection.db.yelp_review_scored_pairs.find(query, what))
+    map(for_each_review_, processed)
     processed = pd.DataFrame(processed).set_index('review_id')
 
     ret = processed.reset_index().T.to_dict()
